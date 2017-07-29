d0044da62c85fcfc862f72c99b2181ec7f087bbb
otofu-square <yukita@feedforce.jp>
2017-07-29 23:51:43 +0900

    :recycle: Refactor by using functional component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,31 +2,16 @@ import * as React from "react";
 
 interface IProps {
   messages: string[];
-  channel: any;
 }
 
-class App extends React.Component<IProps, {}> {
-  public messages: string[];
-  public channel: any;
-
-  constructor(props: IProps) {
-    super();
-    this.messages = props.messages;
-    this.channel = props.channel;
-  }
-
-  public render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        {this.messages.map(message =>
-          <p key={message}>
-            {message}
-          </p>,
-        )}
-      </div>
-    );
-  }
-}
+const App = ({ messages }: IProps) =>
+  <div>
+    <h1>Hello World</h1>
+    {messages.map(message =>
+      <p key={message}>
+        {message}
+      </p>,
+    )}
+  </div>;
 
 export default App;
